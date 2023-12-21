11bbc724be7dd083a99b4d674f6338578e1b7473
Irina Usacheva <fantasticnone@yandex.ru>
2023-12-21 18:14:32 +0300

    feat:add booking (not ready)

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -5,7 +5,6 @@ import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.*;
-import java.security.Timestamp;
 import java.time.LocalDateTime;
 
 @Entity
