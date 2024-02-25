1c2ecd586e57bcd673a9ebeb9cf36c1a4e93a44b
Irina Usacheva <fantasticnone@yandex.ru>
2024-02-25 19:52:08 +0300

    request+tests

--- src/test/java/ru/practicum/shareit/booking/service/BookingServiceUnitTest.java
+++ src/test/java/ru/practicum/shareit/booking/service/BookingServiceUnitTest.java
@@ -1,46 +1,33 @@
 package ru.practicum.shareit.booking.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
+
 import ru.practicum.shareit.booking.*;
 import ru.practicum.shareit.booking.dto.BookingRequestDto;
 import ru.practicum.shareit.booking.dto.BookingDto;
-import ru.practicum.shareit.exception.NotFoundException;
-import ru.practicum.shareit.exception.ItemBookingException;
-import ru.practicum.shareit.booking.dto.mapper.BookingMapper;
+
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.ItemRepository;
-import ru.practicum.shareit.exception.DataException;
-import ru.practicum.shareit.item.service.ItemService;
+
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
-import ru.practicum.shareit.user.service.UserService;
+
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
-import static ru.practicum.shareit.booking.BookingStatus.WAITING;
+
 
 @ExtendWith(MockitoExtension.class)
-class BookingServiceImplTest {
+class BookingServiceUnitTest {
 
     @Mock
     private BookingRepository bookingRepository;
